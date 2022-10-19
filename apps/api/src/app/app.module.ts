fe3c4b2632737c64a2f0f31aef2fbe5d4355afcc
yupix <yupi0982@outlook.jp>
2022-10-19 21:49:20 +0900

    feat: TypeORMの接続を作るように

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from '@ngs-market/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config.db.host,
+      port: config.db.port,
+      username: config.db.user,
+      password: config.db.pass,
+      database: config.db.db,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
