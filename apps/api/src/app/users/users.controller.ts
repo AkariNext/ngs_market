0f32744a29f1b3f1f5380b20b138e65533fd61ed
yupix <yupi0982@outlook.jp>
2022-11-07 21:26:15 +0900

    feat: ユーザーidからプロフィールを取得する

--- apps/api/src/app/users/users.controller.ts
+++ apps/api/src/app/users/users.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Users } from '@ngs-market/db';
 import { AuthGuard } from '../auth.guard';
 import { CurrentUser } from '../current-user.decorator';
 import { CurrentUserInterceptor } from '../current-user.interceptor';
+import { RemovePasswordInterceptor } from '../remove-pass';
+import { UsersService } from './users.service';
 
 @UseInterceptors(CurrentUserInterceptor)
 @Controller('users')
 export class UsersController {
-  @Get('/:whoami')
+  constructor(
+    private usersService: UsersService
+  ) {}
+  @Get('/whoami')
   @UseGuards(AuthGuard)
   whoAmi(@CurrentUser() user: Users) {
     console.log(user)
+  }
+
+  @Get(':userId')
+  async getProfile(@Param('userId') userId: string) {
+    return await this.usersService.findOne(userId)
   }
 }
